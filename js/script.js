67e9656d7c6b72a9236c872460542f662bec1bdd
User <01txxt10@gmail.com>
2023-01-15 15:03:25 +0700

    Update 2023-01-15T08:03:14.749Z

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-const navigation = document.querySelector('.navigation');
-const navToggle = document.querySelector('.navigation__toggle');
-
-navigation.classList.remove('navigation--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navigation.classList.contains('navigation--closed')) {
-    navigation.classList.remove('navigation--closed');
-    navigation.classList.add('navigation--opened');
-  } else {
-    navigation.classList.add('navigation--closed');
-    navigation.classList.remove('navigation--opened');
-  }
-});
+const navigation=document.querySelector(".navigation"),navToggle=document.querySelector(".navigation__toggle");navigation.classList.remove("navigation--nojs"),navToggle.addEventListener("click",(function(){navigation.classList.contains("navigation--closed")?(navigation.classList.remove("navigation--closed"),navigation.classList.add("navigation--opened")):(navigation.classList.add("navigation--closed"),navigation.classList.remove("navigation--opened"))}));
